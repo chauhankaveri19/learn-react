1cc10a46cacc31f623100a213431b6904b73bd79
chauhankaveri19 <chauhankaveri05@gmail.com>
2026-06-30 20:31:32 +0530

    components

---
+++ 02-components/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+    import React from 'react'
+
+    const Navbar = () => {
+    return (
+        <div>
+            <div className='nav'>
+        <h1>kaveri chauhan</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h3>
+        </div>
+        </div>
+    )
+    }
+
+    export default Navbar
---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <div>
+       <div className='card'>
+      <h1>kaveri chauhan</h1>
+      <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</h3>
+    </div>
+    </div>
+  )
+}
+
+export default card
