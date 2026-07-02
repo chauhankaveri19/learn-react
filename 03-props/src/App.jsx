084b1e38425dcd036b60b0f7331298931ac9ee76
chauhankaveri19 <chauhankaveri05@gmail.com>
2026-07-02 20:53:23 +0530

    "learn props"

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Card from './components/card'
+
+const App = () => {
+  return (
+    <div>
+      <div className="parent">
+        
+     <Card user='priyanka' age={20} img='https://static.vecteezy.com/system/resources/thumbnails/036/227/351/small_2x/ai-generated-nature-landscapes-background-free-photo.jpg' />
+       <Card user='vedanshi' age={25} img='https://tse4.mm.bing.net/th/id/OIP.Sub4rPjVLwqUZa__LLea1QHaEO?cb=thfc1falcon4&pid=ImgDet&w=185&h=105&c=7&dpr=1.3&o=7&rm=3' />
+
+
+
+
+    </div>
+  
+    </div>
+    
+  )
+}
+
+export default App
